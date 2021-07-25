3afad46273b24040ba3bef5ff07c7883baf039f5
Huvana <bennyangiye202@gmail.com>
2021-07-25 20:09:25 +0300

    new

---
+++ files/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms import validators
+from wtforms.validators import Length,EqualTo,Email,DataRequired, ValidationError
+from files.models import user
+
+class RegisterForm(FlaskForm):
+    def validate_username(self, username_to_check):
+        User = user.query.filter_by(username=username_to_check.data).first()
+        if User:
+            raise ValidationError('Username already exists! Please try a different username')
+
+    def validate_email_address(self, email_address_to_check):
+        email_address = user.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Email Address already exists! Please try a different email address')
+
+    username=StringField(label='Username:',validators=[Length(min=2,max=10),DataRequired()])
+    email_address=StringField(label='Email-Address:',validators=[Email(),DataRequired()])
+    password1=PasswordField(label='Password:',validators=[Length(min=6),DataRequired()])
+    password2=PasswordField(label='Confirm password:',validators=[EqualTo('password1'),DataRequired()])
+    submit=SubmitField(label='Create Account')
